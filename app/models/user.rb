f1f1f8d62dc0efe6f3bec415eab98eee059d5859
Yoshinori Kawasaki <ykawasaki@gmail.com>
2012-10-13 13:21:47 +0900

    Add qiita fetch and show it

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,15 @@ class User < ActiveRecord::Base
   has_many :taggings
   has_many :tags, through: :taggings
 
-  attr_accessible :name
+  attr_accessible :url_name
 
-  def fetch_user_item
-    Qiita.user
+  def fetch_qiita_data
+    raw_user = Qiita.user url_name
+    logger.info raw_user
+    raw_user.each do |key, value|
+      self[key.to_sym] = value
+    end
+    save!
   end
 
 end
