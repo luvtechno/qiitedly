e1750fa521d2dff4a0d896ea538d4a75eff39cf1
Yoshinori Kawasaki <ykawasaki@gmail.com>
2012-10-13 12:46:30 +0900

    Add a script to get and save tags

---
+++ lib/batch/create_tags.rb
@@ -0,0 +1,9 @@
+Qiita.tags.each do |raw_tag|
+  tag = Tag.where(name: raw_tag.name).first_or_initialize
+  tag.url_name = raw_tag.url_name
+  tag.icon_url = raw_tag.icon_url
+  tag.item_count = raw_tag.item_count
+  tag.follower_count = raw_tag.follower_count
+  tag.save!
+  puts "Saved #{tag.name}"
+end
