37c63f8466d35dc9195855bd3bac8a1aaa66040c
Yoshinori Kawasaki <ykawasaki@gmail.com>
2012-10-14 00:17:16 +0900

    Add more utm params

--- app/models/search_result.rb
+++ app/models/search_result.rb
@@ -2,7 +2,7 @@ class SearchResult < ActiveRecord::Base
   belongs_to :tag
 
   def url
-    utm = { utm_campaign: :qiitedly }
+    utm = { utm_medium: :web, utm_source: :qiitedly, utm_campaign: :qiitedly }
     "#{link}?#{utm.to_param}"
   end
 end
